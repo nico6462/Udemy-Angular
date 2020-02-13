ffc311d2646990bb47aae4a472673b94ece4364e
NPaulus <nico6462@gmail.com>
2020-02-13 11:22:57 +0100

    Set Course

--- first-project/my-first-app/src/app/app.module.ts
+++ first-project/my-first-app/src/app/app.module.ts
@@ -4,16 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { SuccesAlertComponent } from './succes-alert/succes-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent,
-    WarningAlertComponent,
-    SuccesAlertComponent
+    ServersComponent
   ],
   imports: [
     BrowserModule
