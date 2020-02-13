cb531cbcfa1976476283742823aadf96c7ab32dc
NPaulus <nico6462@gmail.com>
2020-02-13 12:02:36 +0100

    Learning Databinding

--- first-project/my-first-app/src/app/servers/servers.component.ts
+++ first-project/my-first-app/src/app/servers/servers.component.ts
@@ -11,10 +11,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'TestServer';
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+   }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: any){
+    console.log(event);
+    this.serverName = event.target.value;
   }
 
 }
