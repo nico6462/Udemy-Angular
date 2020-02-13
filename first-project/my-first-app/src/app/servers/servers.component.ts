b69f7ecb7d6a0c1eebacd122f9dd43a4141a2dfb
NPaulus <nico6462@gmail.com>
2020-02-13 13:09:07 +0100

    exemple ngif else

--- first-project/my-first-app/src/app/servers/servers.component.ts
+++ first-project/my-first-app/src/app/servers/servers.component.ts
@@ -14,7 +14,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'TestServer';
-  userName = '';
+  serverCreated = false;
 
   constructor() {
     setTimeout(() => {
@@ -28,6 +28,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer(){
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+    this.serverCreated = true;
   }
 
   onUpdateServerName(event: any){
